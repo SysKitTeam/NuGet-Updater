2399c6aa2674d8862bf224baef0c92c7bffabfb5
isaric <igor.saric@acceleratio.net>
2016-09-20 11:22:30 +0200

    changed nuget binary path

--- Acceleratio.Nuget.Updater/Constants.cs
+++ Acceleratio.Nuget.Updater/Constants.cs
@@ -2,7 +2,7 @@
 {
     public sealed class Constants
     {       
-        public static string NuGetWebBinary => "https://github.com/Acceleratio/NuGet-Updater/raw/master/Acceleratio.Nuget.Updater/NugetBinary/nuget.exe";
+        public static string NuGetWebBinary => "https://github.com/Acceleratio/NuGet-Updater/raw/master/NugetBinary/nuget.exe";
         public static string DefaultRepository => "http://nuget.acceleratio.hr/nuget/";
         public static string NuGetBinary => "nuget.exe";
         public static string AppVersion => "1.2";
